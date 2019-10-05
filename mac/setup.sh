2bf516e59216233a6c710ed8f8e708d1ba705352
7urkm3n <rgurdov@gmail.com>
2019-10-05 14:40:27 -0400

    setup.sh cleaned

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,12 +1,4 @@
 # NEED TO WORK ON THIS FILE !!!
-# Tasks:
-# 1) Brew installation and dependencies from .custom profile file
-# 2) brew install cmake also supposed to be added
-# 3) cp command doesnt work in this file check init() function!
-# 4) install GVM-golang `bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)`
-# 5) install NVM https://github.com/creationix/nvm#install-script
-# 6) install RVM \curl -sSL https://get.rvm.io | bash
-
 ########### 
 # scutil --set ComputerName "7urkm3n"
 # scutil --set LocalHostName "7urkm3n"
@@ -21,20 +13,86 @@ function git_aware_prompt() {
 }
 
 
-function install_gems(){
+function install_gems() {
 	if ! [ -x "$(command -v foreman)" ]; then
 	  echo "Foreman wasn't installed, Instaling foreman ..."
 	  gem install foreman
 	fi
 }
 
+function brew_installation() {
+  echo "Installing Brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
 
-function init(){
-  scutil --set ComputerName "7urkm3n";
-  scutil --set LocalHostName "7urkm3n";
-  scutil --set HostName "7urkm3n";
+function brew_install_cmake() {
+  echo "Installing `cmake` via brew"
+  brew install cmake
+}
 
-  git_aware_prompt();
+function brew_install_git_auto_complete() {
+  # Git auto complete
+  brew install git && brew install bash-completion
+}
+
+function install_gvm() {
+  echo "Installing `gvm`"
+  bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+}
+
+function install_rvm() {
+  echo "Installing `rvm`"
+  \curl -sSL https://get.rvm.io | bash
+}
+
+function install_nvm() {
+  echo "Installing `nvm`"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+}
+
+function install_dbs() {
+  echo "Installing Mongodb" && brew install mongodb
+  echo "Installing Postgres" && brew install postgres
+  echo "Installing Mysql" && brew install mysql
+  echo "Installing Redis" && brew install redis
+  echo "Installing Rethinkdb" && brew install rethinkdb
+  echo "Installing Mailhog" && brew install mailhog
+  echo "Installing Elasticsearch" && brew install elasticsearch
+}
+
+
+
+function current_machine_username() {
+  echo "Cheching computer, host and localhost names"
+
+  name="7urkm3n"
+  if [ `scutil --get ComputerName` != $name ]
+  then
+    echo "Setting ComputerName to $name"
+    scutil --set ComputerName $name;
+  fi
+
+  if [ `scutil --get LocalHostName` != $name ]
+  then
+    echo "Setting LocalHostName to $name"
+    scutil --set LocalHostName $name;
+  fi
+
+  if [ `scutil --get HostName` != $name ]
+  then
+    echo "Setting HostName to $name"
+    scutil --set HostName $name;
+  fi
+}
+
+
+function init() {
+  echo "Init Executed"
+  installing_brew
+  current_machine_username
+  install_dbs
+  # git_aware_prompt()
   # cp .custom_profile ~/
 }
-init();
+
+init
